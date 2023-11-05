bda399100e24f8f34d39aedd25765d2a125aad21
Padma Patil <padma@email.com>
2023-11-05 08:30:27 -0800

    add variables set to value of html elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 window.onload = function() {
-    
+
+const form = document.querySelectory("form");
+form.onsubmit = function(event) {
+    event.preventDefault();
+
+    // access HTML elements and gather age value
+
+    const age = parseInt(document.querySelector("input#age").value);
+
+    let vegetarian = document.getElementById("vegetarian").value
+    let educationLevel = document.getElementById("education").value;
+    let snackType= document.getElementById('snack').value;
+    let energyLevel= document.getElementById("energy").value;
+ 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
